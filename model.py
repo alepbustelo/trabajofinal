4f5ec7c7a3bd3c45d2d81111697eed08f1902e20
alepbustelo <109233035+alepbustelo@users.noreply.github.com>
2023-02-21 14:46:55 -0300

    Update model.py

--- model.py
+++ model.py
@@ -131,7 +131,7 @@ elif selected == 'Model':
             st.dataframe(trial_data)
 
             # LLamo al pkl con el modelo
-            with open('./financial_inc.pkl', 'rb') as clf_inclusion:
+            with open('./financial_inclusion.pkl', 'rb') as clf_inclusion:
                 modelo_inclusion = pickle.load(clf_inclusion)
             # Prediccion usando el trial data con lo insertado en el form
             if modelo_inclusion.predict(trial_data) == 1:
